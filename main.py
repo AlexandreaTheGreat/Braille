c1f2e3aeaae960d39e0fa25d23eeae8a6844ebbf
AlexandreaTheGreat <134761954+AlexandreaTheGreat@users.noreply.github.com>
2023-05-27 23:01:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,6 @@ else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
-    class_names: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    class_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
